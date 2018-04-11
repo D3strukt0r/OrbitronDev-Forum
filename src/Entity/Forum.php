b498a9a4874d533d5169573c70648969b8ff8df4
Manuele Vaccari <manuele.vaccari@gmail.com>
2018-04-11 18:22:45 +0200

    Update 17
    * Fixed problem with old MySql server (SQLSTATE[42000]: Syntax error or access violation: 1071 Specified key was too long; max key length is 767 bytes)

--- src/Entity/Forum.php
+++ src/Entity/Forum.php
@@ -21,13 +21,13 @@ class Forum
 
     /**
      * @var string
-     * @ORM\Column(type="string", unique=true)
+     * @ORM\Column(type="string", unique=true, length=191)
      */
     protected $name;
 
     /**
      * @var string
-     * @ORM\Column(type="string", unique=true)
+     * @ORM\Column(type="string", unique=true, length=191)
      */
     protected $url;
 
